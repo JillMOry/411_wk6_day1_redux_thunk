3df7c5cc383a62f4610883489f7f1657c8758475
JillMOry <oryjill@gmail.com>
2020-06-25 20:51:26 -0500

    thunkcommit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow
+} from "@material-ui/core";
+
+const options = ["Delete"];
+
+const ITEM_HEIGHT = 48;
 
 const Import = (props) => {
-    // fill out this component
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-export default Import
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div>
+			<p>{props.makes.length}</p>
+			<button variant="contained" color="primary" onClick={props.fetchMakes}>
+				Import
+			</button>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell align="right">ID</TableCell>
+						<TableCell align="right">Make</TableCell>
+						<TableCell align="right">Actions</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{props.makes.map((make, index) => {
+						return (
+							<TableRow key={make.MakeId}>
+								<TableCell align="right">{make.MakeId}</TableCell>
+								<TableCell align="right">{make.MakeName}</TableCell>
+								<TableCell align="right">
+									<IconButton
+										aria-label="more"
+										aria-controls="long-menu"
+										aria-haspopup="true"
+										onClick={handleClick}
+									>
+										<MoreVertIcon />
+									</IconButton>
+									<Menu
+										id="long-menu"
+										anchorEl={anchorEl}
+										keepMounted
+										open={open}
+										onClose={handleClose}
+										PaperProps={{
+											style: {
+												maxHeight: ITEM_HEIGHT * 4.5,
+												width: 200
+											}
+										}}
+									>
+										{/* <MenuItem key="1" >
+											delete
+										</MenuItem> */}
+										{options.map((option) => (
+											<MenuItem
+												key={option}
+												selected={option === "Delete"}
+												onClick={() => props.deleteMake(index)} //(index)
+											>
+												{console.log(index)}
+												{option}
+											</MenuItem>
+										))}
+									</Menu>
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</div>
+	);
+};
+
+export default Import;
