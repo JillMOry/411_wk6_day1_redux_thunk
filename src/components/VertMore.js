122abab0f21c75a4892bb573693acbf8859391d3
JillMOry <oryjill@gmail.com>
2020-06-27 16:35:24 -0500

    completed assignment

---
+++ src/components/VertMore.js
@@ -0,0 +1,50 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+export default function VertMore(props) {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const handleDelete = () => {
+		setAnchorEl(null);
+		props.deleteMake(props.index);
+	};
+
+	return (
+		<div>
+			<IconButton
+				aria-label="more"
+				aria-controls="long-menu"
+				aria-haspopup="true"
+				onClick={handleClick}
+			>
+				<MoreVertIcon />
+			</IconButton>
+			<Menu
+				id="long-menu"
+				anchorEl={anchorEl}
+				keepMounted
+				open={open}
+				onClose={handleClose}
+				PaperProps={{
+					style: {
+						width: "20ch"
+					}
+				}}
+			>
+				<MenuItem onClick={handleDelete}>Delete</MenuItem>
+			</Menu>
+		</div>
+	);
+}
